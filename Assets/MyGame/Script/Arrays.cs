981e03a1de2fc0e60b165c53591bfe9d22ec2d03
pklacher <pia.lacher04@htl-salzburg.ac.at>
2021-05-31 13:14:37 +0200

    add inclass

--- Assets/MyGame/Script/Arrays.cs
+++ Assets/MyGame/Script/Arrays.cs
@@ -10,6 +10,9 @@ public class Arrays : MonoBehaviour
     public int[] array3 = { 31, 32, 33, 34, 35, 36, 37 };
     public string[] array4 = new string[10];
 
+    //"HÜ"
+    public int[] array32 = new int[] { 31, 32, 33, 34, 35, 36, 37 };
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +25,12 @@ public class Arrays : MonoBehaviour
         }
 
         arrayColoredImagesRed = GameObject.FindGameObjectsWithTag("RedImage");
+        Debug.Log(arrayColoredImagesRed.Length);
+
+        foreach (GameObject elem in arrayColoredImagesRed)
+        {
+            Debug.Log(elem.name);
+        }
 
         Debug.Log("Element 0 von array 1 hat den Wert: " + array1[0]);
         Debug.Log("Element 0 von array 2 hat den Wert: " + array2[0]);
